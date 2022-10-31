2320d192b313cc46d05e0933c2400ea6da8c5ba2
AnAvgGuy <97361188+AnAvgGuy@users.noreply.github.com>
2022-10-31 22:31:52 +0000

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,20 +1,53 @@
 import pyxel
+#from random import randint as rn
+#import math as m
 
+MY_PI = 3.141592
+
+class Player():
+    def __init__(self):
+        self.x, self.y = 0, 0
+        self.vel = 1.5
+        self.angle = 0
+    def move(self):
+        self.vx, self.vy = 0, 0
+
+        if pyxel.btn(pyxel.KEY_D):
+            self.vx += self.vel
+        if pyxel.btn(pyxel.KEY_A):
+            self.vx -= self.vel
+
+        if pyxel.btn(pyxel.KEY_S):
+            self.vy += self.vel
+        if pyxel.btn(pyxel.KEY_W):
+            self.vy -= self.vel
+        if not (self.vx == 0 and self.vy == 0):
+            ag = pyxel.atan2(self.vy,self.vx)
+            self.x += int(self.vel * pyxel.cos(ag))
+            self.y += int(self.vel * pyxel.sin(ag))
+    def draw(self):
+
+        pyxel.rect(self.x, self.y, 4, 4, 2)
+        '''self.angle += 0.05
+        a, b = self.x + 18*pyxel.cos(self.angle), self.y + 18*pyxel.sin(self.angle)
+        c, d = self.x + 18*pyxel.cos(self.angle+MY_PI/3*2), self.y + 18*pyxel.sin(self.angle+MY_PI/3*2)
+        e, f = self.x + 18*pyxel.cos(self.angle+MY_PI/3*4), self.y + 18*pyxel.sin(self.angle+MY_PI/3*4)
+
+        pyxel.tri(a, b, c, d, e, f, 1)'''
 
 class App:
     def __init__(self):
-        pyxel.init(160, 120, title="Hello Pyxel")
-        pyxel.image(0).load(0, 0, "assets/pyxel_logo_38x16.png")
+        pyxel.init(160, 120,title="   something",fps=60)
+        
+        self.player = Player()
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        if pyxel.btnp(pyxel.KEY_Q):
-            pyxel.quit()
+        self.player.move()
+
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
-        pyxel.blt(61, 66, 0, 0, 0, 38, 16)
-
+        self.player.draw()
 
 App()
